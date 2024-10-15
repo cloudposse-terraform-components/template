f7a95de69508f64e1e8d871f9e0862933b6cb795
Igor Rodionov <goruha@gmail.com>
2024-10-15 23:55:24 +0300

    Update outputs.tf

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,4 @@
 output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template."
+  description = "Mock output example for the Cloud Posse Terraform component template"
   value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
 }
