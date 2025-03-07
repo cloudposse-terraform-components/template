841b492a02f4a3563f0ff6b9b38d30237f116585
Igor Rodionov <goruha@gmail.com>
2025-03-07 15:30:09 +0300

    Add tests (#49)
    * Added example
    
    * Minimize templates

---
+++ test/component_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"testing"
+
+	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
+	"github.com/stretchr/testify/assert"
+)
+
+type ComponentSuite struct {
+	helper.TestSuite
+}
+
+func (s *ComponentSuite) TestBasic() {
+	const component = "example/basic"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
+
+	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+	assert.NotNil(s.T(), options)
+
+	s.DriftTest(component, stack, nil)
+}
+
+
+func (s *ComponentSuite) TestEnabledFlag() {
+	const component = "example/disabled"
+	const stack = "default-test"
+	s.VerifyEnabledFlag(component, stack, nil)
+}
+
+func TestRunSuite(t *testing.T) {
+	suite := new(ComponentSuite)
+	helper.Run(t, suite)
+}
