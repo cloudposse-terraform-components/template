aef46254f10a94943c9e64287fbcb29203735cc0
Igor Rodionov <goruha@gmail.com>
2025-04-26 17:14:48 +0300

    Test mergify 9

--- src/outputs.tf
+++ src/outputs.tf
@@ -2,3 +2,7 @@ output "mock" {
   description = "Mock output example for the Cloud Posse Terraform component template"
   value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
 }
+
+
+
+
